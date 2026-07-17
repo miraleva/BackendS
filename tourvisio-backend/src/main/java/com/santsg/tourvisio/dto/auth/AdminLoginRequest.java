e6cbd89dba257741bd0c329317189b4ceee10aa0
irmakozbay <irmakozbay05@gmail.com>
2026-07-17 10:39:46 +0300

    admin authentication added

---
+++ tourvisio-backend/src/main/java/com/santsg/tourvisio/dto/auth/AdminLoginRequest.java
@@ -0,0 +1,14 @@
+package com.santsg.tourvisio.dto.auth;
+
+public class AdminLoginRequest {
+    private String password;
+
+    // Getter ve Setter metotları
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
