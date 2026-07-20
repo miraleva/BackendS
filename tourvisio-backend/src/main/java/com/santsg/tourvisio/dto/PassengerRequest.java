763577c3e539f1f9e8851de96c4dadb6f7301826
zellylz <yorulmazelal@gmail.com>
2026-07-20 10:54:50 +0300

    feat: add support for collecting all passenger details during reservation

--- tourvisio-backend/src/main/java/com/santsg/tourvisio/dto/PassengerRequest.java
+++ tourvisio-backend/src/main/java/com/santsg/tourvisio/dto/PassengerRequest.java
@@ -2,6 +2,8 @@ package com.santsg.tourvisio.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,7 @@ public class PassengerRequest {
     private String lastName;
 
     @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
 
     @NotBlank(message = "Phone number cannot be blank")
@@ -25,4 +28,25 @@ public class PassengerRequest {
 
     @NotBlank(message = "Identity number (TC No) cannot be blank")
     private String identityNumber;
+
+    @NotNull(message = "Birth date cannot be null")
+    private LocalDate birthDate;
+
+    @NotBlank(message = "Gender cannot be blank")
+    private String gender;
+
+    @NotBlank(message = "Nationality cannot be blank")
+    private String nationality;
+
+    // 5-argument constructor for test compatibility
+    public PassengerRequest(String firstName, String lastName, String email, String phoneNumber, String identityNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.identityNumber = identityNumber;
+        this.birthDate = LocalDate.of(1990, 1, 1);
+        this.gender = "MR";
+        this.nationality = "TR";
+    }
 }
